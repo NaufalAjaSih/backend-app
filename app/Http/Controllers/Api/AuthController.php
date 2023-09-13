2fe18d4c6730a3ade37a1d50ac05b35bf6fcdb0f
NaufalAjaSih <masanaufal@gmail.com>
2023-09-13 10:55:20 +0700

    adding register & logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,9 +6,32 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user',
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+        return response()->json([
+            'message' => 'Register Success',
+            'token' => $token
+        ]);
+    }
+
     public function login(Request $request)
     {
         $request->validate([
@@ -32,6 +55,14 @@ class AuthController extends Controller
         }
         return response()->json([
             'message' => 'Invalid login credentials'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Logout Success'
         ]);
     }
 }
